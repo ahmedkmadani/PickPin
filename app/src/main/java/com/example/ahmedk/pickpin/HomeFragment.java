93d9ae7fc405db1aee94429d6bf0bc4c13752b98
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-02-07 22:11:09 +0000

    add float button to home fragment

--- app/src/main/java/com/example/ahmedk/pickpin/HomeFragment.java
+++ app/src/main/java/com/example/ahmedk/pickpin/HomeFragment.java
@@ -7,6 +7,8 @@ import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.DisplayMetrics;
@@ -41,6 +43,15 @@ public  class HomeFragment extends Fragment implements ClusterManager.OnClusterI
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
