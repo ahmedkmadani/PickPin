756ac7fcd55018c208f5a548fab80e4d96b14614
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-02-08 00:23:26 +0000

    adding permission for camera using dexter lib

--- app/src/main/java/com/example/ahmedk/pickpin/HomeFragment.java
+++ app/src/main/java/com/example/ahmedk/pickpin/HomeFragment.java
@@ -1,16 +1,27 @@
 package com.example.ahmedk.pickpin;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.graphics.Canvas;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,6 +39,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.ClusterManager;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -36,6 +55,7 @@ public  class HomeFragment extends Fragment implements ClusterManager.OnClusterI
     private ClusterManager<PinsItem> mClusterManager;
     private PinsItem clusterItem;
     private GoogleMap mMap;
+
 
     @Nullable
     @Override
@@ -49,6 +69,8 @@ public  class HomeFragment extends Fragment implements ClusterManager.OnClusterI
             @Override
             public void onClick(View v) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+                requestCameraPermission();
             }
         });
 
@@ -62,6 +84,63 @@ public  class HomeFragment extends Fragment implements ClusterManager.OnClusterI
         });
 
         return view;
+    }
+
+    private void requestCameraPermission() {
+        Dexter.withActivity(getActivity()).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                OpenCamera();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                if(response.isPermanentlyDenied()){
+                    ShowSettingsDialog();
+                }
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                token.cancelPermissionRequest();
+            }
+        }).withErrorListener(new PermissionRequestErrorListener() {
+            @Override
+            public void onError(DexterError error) {
+                Toast.makeText(getActivity(), "Error occurred! ", Toast.LENGTH_SHORT).show();
+            }
+        }).onSameThread().check();
+    }
+
+    private void ShowSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Need Permissions");
+        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.");
+        builder.setPositiveButton("GOTO SETTINGS", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                openSettings();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+
+    private void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getActivity().getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, 101);
+    }
+
+    private void OpenCamera() {
+        Toast.makeText(getActivity(), "Open Camera ", Toast.LENGTH_SHORT).show();
     }
 
     private void setUpCluster() {
