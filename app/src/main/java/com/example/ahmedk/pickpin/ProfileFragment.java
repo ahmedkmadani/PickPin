f1508ae41a0fd985ab7d73644388880dd2aa9f8d
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-02-09 18:08:48 +0000

    adding change profile image feature

--- app/src/main/java/com/example/ahmedk/pickpin/ProfileFragment.java
+++ app/src/main/java/com/example/ahmedk/pickpin/ProfileFragment.java
@@ -1,58 +1,167 @@
 package com.example.ahmedk.pickpin;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.DexterBuilder;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.io.IOException;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ProfileFragment  extends Fragment {
-
-    public static final String INTENT_IMAGE_PICKER_OPTION = "image_picker_option";
-    public static final String INTENT_ASPECT_RATIO_X = "aspect_ratio_x";
-    public static final String INTENT_ASPECT_RATIO_Y = "aspect_ratio_Y";
-    public static final String INTENT_LOCK_ASPECT_RATIO = "lock_aspect_ratio";
-    public static final String INTENT_IMAGE_COMPRESSION_QUALITY = "compression_quality";
-    public static final String INTENT_SET_BITMAP_MAX_WIDTH_HEIGHT = "set_bitmap_max_width_height";
-    public static final String INTENT_BITMAP_MAX_WIDTH = "max_width";
-    public static final String INTENT_BITMAP_MAX_HEIGHT = "max_height";
-
-
-    public static final int REQUEST_IMAGE_CAPTURE = 0;
-    public static final int REQUEST_GALLERY_IMAGE = 1;
-
-    private boolean lockAspectRatio = false, setBitmapMaxWidthHeight = false;
-    private int ASPECT_RATIO_X = 16, ASPECT_RATIO_Y = 9, bitmapMaxWidth = 1000, bitmapMaxHeight = 1000;
-    private int IMAGE_COMPRESSION = 80;
-    public static String fileName;
-
-
+    private static final String TAG = ProfileFragment.class.getSimpleName();
+    public int REQUEST_IMAGE = 100;
+    ImageView imgProfile;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(getActivity());
 
-        Intent intent = getActivity().getIntent();
+        imgProfile = (ImageView) view.findViewById(R.id.img_profile);
+        loadProfileDefault();
+        ImagePickerActivity.clearCache(getActivity());
 
-        if (intent == null){
-            Toast.makeText(getActivity(), getString(R.string.toast_image_intent_null), Toast.LENGTH_LONG).show();
-            return view;
-        }
+        imgProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withActivity(getActivity())
+                        .withPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .withListener(new MultiplePermissionsListener() {
+                            @Override
+                            public void onPermissionsChecked(MultiplePermissionsReport report) {
+                                if (report.areAllPermissionsGranted()) {
+                                    showImagePickerOptions();
+                                }
 
-        ASPECT_RATIO_X = intent.getIntExtra(INTENT_ASPECT_RATIO_X, ASPECT_RATIO_X);
-        ASPECT_RATIO_Y = intent.getIntExtra(INTENT_ASPECT_RATIO_Y, ASPECT_RATIO_Y);
-        IMAGE_COMPRESSION = intent.getIntExtra(INTENT_IMAGE_COMPRESSION_QUALITY, IMAGE_COMPRESSION);
-        lockAspectRatio = intent.getBooleanExtra(INTENT_LOCK_ASPECT_RATIO, false);
-        setBitmapMaxWidthHeight = intent.getBooleanExtra(INTENT_SET_BITMAP_MAX_WIDTH_HEIGHT, false);
-        bitmapMaxWidth = intent.getIntExtra(INTENT_BITMAP_MAX_WIDTH, bitmapMaxWidth);
-        bitmapMaxHeight = intent.getIntExtra(INTENT_BITMAP_MAX_HEIGHT, bitmapMaxHeight);
+                                if (report.isAnyPermissionPermanentlyDenied()) {
+                                    showSettingsDialog();
+                                }
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                                token.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
 
         return view;
     }
+
+    private void loadProfileDefault() {
+
+        Glide.with(getActivity()).load(R.drawable.baseline_account_circle_black_48)
+                .into(imgProfile);
+        imgProfile.setColorFilter(ContextCompat.getColor(getActivity(), R.color.profile_default_tint));
+    }
+
+    private void loadProfile(String url) {
+        Log.d(TAG, "Image cache path: " + url);
+
+        Glide.with(getActivity()).load(url)
+                .into(imgProfile);
+        imgProfile.setColorFilter(ContextCompat.getColor(getActivity(), android.R.color.transparent));
+    }
+
+    private void showSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(getString(R.string.dialog_permission_title));
+        builder.setMessage(getString(R.string.dialog_permission_message));
+        builder.setPositiveButton(getString(R.string.go_to_settings), (dialog, which) -> {
+            dialog.cancel();
+            openSettings();
+        });
+        builder.setNegativeButton(getString(android.R.string.cancel), (dialog, which) -> dialog.cancel());
+        builder.show();
+
+    }
+
+    private void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getActivity().getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, 101);
+    }
+
+    private void showImagePickerOptions() {
+        ImagePickerActivity.showImagePickerOptions(getActivity(), new ImagePickerActivity.PickerOptionListener() {
+            @Override
+            public void onTakeCameraSelected() {
+                launchCmaeraIntent();
+            }
+
+            @Override
+            public void onChooseGallerySelected() {
+                launchGalleryIntent();
+            }
+        });
+    }
+
+    private void launchGalleryIntent() {
+        Intent intent = new Intent(getContext(), ImagePickerActivity.class);
+        intent.putExtra(ImagePickerActivity.INTENT_IMAGE_PICKER_OPTION, ImagePickerActivity.REQUEST_GALLERY_IMAGE);
+
+        intent.putExtra(ImagePickerActivity.INTENT_LOCK_ASPECT_RATIO, true);
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_X, 1);
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_Y, 1);
+        startActivity(intent);
+    }
+
+    private void launchCmaeraIntent() {
+        Intent intent = new Intent(getContext(), ImagePickerActivity.class);
+        intent.putExtra(ImagePickerActivity.INTENT_IMAGE_PICKER_OPTION, ImagePickerActivity.REQUEST_IMAGE_CAPTURE);
+
+        intent.putExtra(ImagePickerActivity.INTENT_LOCK_ASPECT_RATIO, true);
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_X, 1);
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_Y, 1);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode== REQUEST_IMAGE){
+            if (resultCode == Activity.RESULT_OK){
+                Uri uri = data.getParcelableExtra("path");
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+                    loadProfile(uri.toString());
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
