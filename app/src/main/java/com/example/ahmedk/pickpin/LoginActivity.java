13ce5d8a50aa2d41ea06c54da5696cbcd2c2d509
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-02-16 23:23:49 +0000

    adding login xml into the project

---
+++ app/src/main/java/com/example/ahmedk/pickpin/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.ahmedk.pickpin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button login = (Button) findViewById(R.id.btn_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
